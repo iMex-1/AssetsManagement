aada52952c06f37546a46bea62b02dbe5da94470
Mohamed Farouq Joudate <farouqjoudate@gmail.com>
2026-04-02 12:24:06 +0100

    Fix login route and switch to MySQL

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 
 Route::resource('users', UserController::class);
 Route::resource('roles', RoleController::class);
